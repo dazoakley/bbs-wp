aac86e522fdd5a4f0893ac70895bf74829cf5819
Darren Oakley <daz.oakley@gmail.com>
2013-12-22 22:47:56 +0000

    Add support for the wp_nav_menu.

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
-<?php  
-if ( function_exists('register_sidebar') )  
-    register_sidebar(array(  
-        'before_widget' => '',  
-        'after_widget' => '',  
-        'before_title' => '<h2>',  
-        'after_title' => '</h2>',  
-    ));  
+<?php
+if ( function_exists('register_sidebar') )
+  register_sidebar(array(
+    'before_widget' => '',
+    'after_widget' => '',
+    'before_title' => '<h2>',
+    'after_title' => '</h2>',
+  )
+);
+
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
 ?>
